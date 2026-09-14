45ee01e851e414606cd1a459fa664272d314a8dd
Somthing <karadeabhishek10@gmail.com>
2026-09-14 17:43:06 +0530

    Refactor increment/decrement examples and add char loop
    Comment out increment and decrement operator examples, and add a loop to print characters from 'a' to 'z'.

--- Loop Contol Instructon/Increment_Decrement_operators.c
+++ Loop Contol Instructon/Increment_Decrement_operators.c
@@ -2,7 +2,7 @@
 int main() {
     // increment operator
     // i++ ++i
-    int i = 1;
+    //int i = 1;
     //printf("%d \n", i++); //use then increment
     //printf("%d \n", ++i); //increment then use
     //printf("%d\n",i);
@@ -10,7 +10,16 @@ int main() {
     // Decrement operator
     // i-- --i
     //printf("%d \n", i--); //use then decrement
-    printf("%d \n", --i); //decrement then use
-    printf("%d",i);
+   // printf("%d \n", --i); //decrement then use
+    //printf("%d",i);
+
+    //for(float i=1.0; i<=10.0; i++) {
+      //  printf("%f \n", i);
+}
+
+      for(char ch='a';ch<='z'; ch++) {
+        printf("%c \n", ch);
+      } 
+    
     return 0;
 }
