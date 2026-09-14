06b9d6f470bf90b369b229438c8e273a7a61fe13
Somthing <karadeabhishek10@gmail.com>
2026-09-14 19:11:26 +0530

    Implement loop to print numbers from 0 to n

---
+++ Loop Contol Instructon/No_from_0_to_n_is_given_user.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int n;
+    printf("enter the value of n:");
+    scanf("%d", &n);
+    
+    // int i=0;
+    // while(i<=n) {
+    //     printf("%d \n", i);
+    //     i++;
+    // }
+  //  2 diff type to solve it
+    for(int i=0; i<=n; i++) {
+        printf("%d \n", i);
+    }
+    return 0;
+}
