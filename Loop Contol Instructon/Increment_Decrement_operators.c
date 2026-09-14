04482a57529b3e8459bb7133d894a55bc57da953
Somthing <karadeabhishek10@gmail.com>
2026-09-14 17:24:12 +0530

    Create Increment_Decrement_operators.c with examples
    Added examples of increment and decrement operators in C.

---
+++ Loop Contol Instructon/Increment_Decrement_operators.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    // increment operator
+    // i++ ++i
+    int i = 1;
+    //printf("%d \n", i++); //use then increment
+    //printf("%d \n", ++i); //increment then use
+    //printf("%d\n",i);
+
+    // Decrement operator
+    // i-- --i
+    //printf("%d \n", i--); //use then decrement
+    printf("%d \n", --i); //decrement then use
+    printf("%d",i);
+    return 0;
+}
