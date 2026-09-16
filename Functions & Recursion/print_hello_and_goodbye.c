d28529833e350160bc42d0fdea0fd2b94b0a71db
Somthing <karadeabhishek10@gmail.com>
2026-09-16 22:53:40 +0530

    Add print_hello_and_goodbye.c with greeting functions

---
+++ Functions & Recursion/print_hello_and_goodbye.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void printHello() ;
+void printGoodbye() ;
+
+int main() {
+    printHello() ;
+    printGoodbye();
+    return 0;
+}
+void printHello() {
+    printf("hello im L \n");
+}
+void printGoodbye() {
+    printf("Goodbye \n");
+}
