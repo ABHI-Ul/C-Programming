9c06969bf0c2558937d6234e6bf71d089b76a472
Somthing <karadeabhishek10@gmail.com>
2026-09-17 20:42:29 +0530

    Update gst _to_the_bill.c

--- Functions & Recursion/gst _to_the_bill.c
+++ Functions & Recursion/gst _to_the_bill.c
@@ -4,11 +4,11 @@ void calculateprint(float value);\
 int main() {
     float value =100;
    calculateprint(value); 
-    printf("value is:%f \n");
+   printf("value is :%f \n",value );
     return 0;
 }
 
 void calculateprint(float value) {
     value = value + (0.18 * value);
-    printf("the final price is :%f",value);
+    printf("the final price is :%f \n",value);
 }
