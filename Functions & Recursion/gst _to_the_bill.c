db2be77f0cf93c4b82703d966299a3acc84c5027
Somthing <karadeabhishek10@gmail.com>
2026-09-17 20:34:57 +0530

    Create gst _to_the_bill.c

---
+++ Functions & Recursion/gst _to_the_bill.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void calculateprint(float value);\
+
+int main() {
+    float value =100;
+   calculateprint(value); 
+    return 0;
+}
+
+void calculateprint(float value) {
+    value = value + (0.18 * value);
+    printf("the final price is :%f",value);
+}
